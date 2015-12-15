127599f52b8dae543f5e4ffa7ac89f9f08e93a61
Fahno <jelles2002@gmail.com>
2015-12-15 14:39:24 +0100

    New Commit

--- pong.js
+++ pong.js
@@ -1,36 +1,59 @@
-var canvas = getElementById("canvas");
+var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var width = window.innerWidth;
-var height = window.innerHeight;
 var targetFPS = 60;
-var delta, then, now;
-var date = new Date();
+var delta = 0, then = 0, now  = 0;
+var gameObjects = [];
 
-var paddle = function() {
-  this.width = 150;
-  this.height = 5;
-  
-  this.x = width / 2 - this.width / 2;
-  this.y = (pos == "top") ? 0 : height; 
-};
+var gameObject = function(x, y, width, height, id) {
+  gameObjects[gameObjects.length] = {
+    x: x,
+    y: y,
+    width: width,
+    height: height,
+    id: id
+  };
+}
 
 var update = function() {
-
+  
 };
 
 var render = function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
   ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, width, height);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+  ctx.fillStyle = "white";
+  for(var i = 0; i < gameObjects.length; i++) {
+    curObject = gameObjects[i];
+    ctx.fillRect(curObject.x, curObject.y, curObject.width, curObject.height);
+  }
+  
 };
 
 var mainGameLoop = function () {
-  now = date.getTime();
+  
+  now = new Date().getTime();
   delta += now - then;
   then = now;
-  
+
   if(delta >= 1000) {
     update();
+    delta = 0;
   }
+  
+  console.log(gameObjects);
+  
   render();
   window.requestAnimationFrame(mainGameLoop);
 };
+
+var init = function() {
+  gameObject(window.innerWidth / 2 - 10, window.innerHeight / 2 - 10, 10, 10, "ball");
+  gameObject(window.innerWidth / 2 - 50, 0, 50, 5, "topPaddle");
+};
+
+init();
+mainGameLoop();
