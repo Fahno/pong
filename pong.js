a4e1b533dbe6ecb55ad33115b156a8995ff8e249
Jelle Spreeuwenberg <Jelle Spreeuwenberg>
2015-12-16 20:49:36 +0100

    Made the game playable

--- pong.js
+++ pong.js
@@ -1,68 +1,180 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var targetFPS = 60;
-var delta = 0, then = 0, now  = 0;
-var gameObjects = [];
-var left = false, right = false, a = false, d = false;
+var then = new Date().getTime(), now, delta = 0;
+var targetUPS = 120;
+var left, right, a, d; 
+var paddleSpeed = 7;
+var topScore = 0, bottomScore = 0;
 
-var gameObject = function(x, y, width, height, id) {
-  gameObjects[gameObjects.length] = {
-    x: x,
-    y: y,
-    width: width,
-    height: height,
-    id: id
-  };
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var ball = {
+	x: canvas.width / 2 - 10 / 2,
+	y: canvas.height / 2 - 10 / 2,
+	width: 10,
+	height: 10,
+	velX: chooseNumber(4, 6, -4, -6),
+	velY: chooseNumber(4, 6, -4, -6),
+	render: function() {
+		ctx.fillStyle = "white";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	},
+	
+	update: function() {
+		this.x += this.velX;
+		this.y += this.velY;
+		
+		if(checkCollision(this, paddleTop) || checkCollision(this, paddleBottom)) {
+			this.velY *= -1;
+		}
+		
+		if(this.x + this.width >= canvas.width || this.x <= 0) {
+			this.velX *= -1;
+		}
+		
+		if(this.y < 0 - this.width || this.y > canvas.height) {
+			topScore += this.y > canvas.height / 2 ? 1 : 0;
+			bottomScore += this.y < canvas.height / 2 ? 1 : 0;
+			this.x = canvas.width / 2 - 10 / 2;
+			this.y = canvas.height / 2 - 10 / 2;
+			this.velX = chooseNumber(4, 6, -4, -6);
+			this.velY = chooseNumber(4, 6, -4, -6);
+			this.velX *= 1.1;
+			this.velY *= 1.1;
+			paddleSpeed *= 1.1;
+		}
+		
+		
+	}
+};
+
+var paddleTop = {
+	x: canvas.width / 2 - 70 / 2,
+	y : 0,
+	width : 70,
+	height : 5,
+	render: function() {
+		ctx.fillStyle = "white";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	},
+	
+	update: function() {
+		this.x -= a === true ? paddleSpeed : 0;
+		this.x += d === true ? paddleSpeed : 0;
+	}
+};
+
+var paddleBottom = {
+	x: canvas.width / 2 - 70 / 2,
+	y : canvas.height - 5,
+	width : 70,
+	height : 5,
+	render: function() {
+		ctx.fillStyle = "white";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	},
+	
+	update: function() {
+		this.x -= left === true ? paddleSpeed : 0;
+		this.x += right === true ? paddleSpeed : 0;
+	}
+};
+
+function chooseNumber(v1, v2, v3, v4) {
+	rand = Math.random();
+	if(rand < 0.25)
+		return v1;
+	else if(rand < 0.50)
+		return v2;
+	else if(rand < 0.75)
+		return v3;
+	else
+		return v4;
 }
 
-var update = function() {
-  console.log("a: " + a + ", right: " + right);
-};
+function checkCollision(ball, paddle) {
+	if (ball.x < paddle.x + paddle.width && ball.x + ball.width > paddle.x && ball.y < paddle.y + paddle.height && ball.height + ball.y > paddle.y) {
+		return true;
+	}
+	return false
+}
 
-var render = function() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
-  ctx.fillStyle = "white";
-  for(var i = 0; i < gameObjects.length; i++) {
-    curObject = gameObjects[i];
-    ctx.fillRect(curObject.x, curObject.y, curObject.width, curObject.height);
-  }
-  
-};
+function mainGameLoop() {
+	now = new Date().getTime();
+	delta += now - then;
+	then = now;
+	
+	if(delta >= 1000 / targetUPS) {
+		update();
+		delta = 0;
+	}
+	
+	render();
+	
+	window.requestAnimationFrame(mainGameLoop);
+}
 
-var mainGameLoop = function () {
-  
-  now = new Date().getTime();
-  delta += now - then;
-  then = now;
+function render() {
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	
+	ball.render();
+	paddleTop.render();
+	paddleBottom.render();
+	
+	ctx.fillStyle = "white";
+	ctx.fillText(bottomScore, 0, canvas.height - 20);
+	ctx.fillText(topScore, 0, 20);
+}
 
-  if(delta >= 1000) {
-    update();
-    delta = 0;
-  }
-  
-  console.log(gameObjects);
-  
-  render();
-  window.requestAnimationFrame(mainGameLoop);
-};
+function update() {
+	ball.update();
+	paddleTop.update();
+	paddleBottom.update();
+}
 
-var init = function() {
-  gameObject(window.innerWidth / 2 - 10 / 2, window.innerHeight / 2 - 10 / 2, 10, 10, "ball");
-  gameObject(window.innerWidth / 2 - 70 / 2, 0, 70, 10, "topPaddle");
-  gameObject(window.innerWidth / 2 - 70 / 2, window.innerHeight - 10, 70, 10, "bottomPaddle");
+window.addEventListener('resize', function() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+});
 
-  document.addEventListener('keydown', function(event) {
-    if(event.keyCode === 37) { left = true } else { left = false }
-    if(event.keyCode === 39) { right = true } else { right = false }
-    if(event.keyCode === 97) { a = true } else { a = false }
-    if(event.keyCode === 100) { d = true } else { d = false }
-  });
-};
+document.addEventListener('keydown', function(event) {
+	switch(event.keyCode) {
+		case 37:
+			left = true;
+			break;
+		case 39:
+			right = true;
+			break;
+		case 65:
+			a = true;
+			break;
+		case 68:
+			d = true;
+			break;
+		default:
+			break;
+	}
+});
 
-init();
+document.addEventListener('keyup', function(event) {
+	switch(event.keyCode) {
+		case 37:
+			left = false;
+			break;
+		case 39:
+			right = false;
+			break;
+		case 65:
+			a = false;
+			break;
+		case 68:
+			d = false;
+			break;
+		default:
+			break;
+	}
+});
+
 mainGameLoop();
